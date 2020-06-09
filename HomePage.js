4a89b9054a43ee404555bd78c3c8d3cf1f49c87a
NupurP21 <64085532+NupurP21@users.noreply.github.com>
2020-06-09 17:03:47 +0100

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -5,3 +5,20 @@ alert("Fantastic, welcome to Travel First Girls! We hope you enjoy reading our b
 else {
 alert ("That's great! Welcome back to Travel First Girls!");
 }
+// Initialize and add the map
+function myMap() {
+    // The location of different places
+    var South_Korea = {lat: 36.5581914, lng: 127.9408564};
+    var New_Zealand = {lat: -41.5000831, lng: 172.8344077};
+    var South_Africa = {lat: -28.8166236, lng: 24.991639};
+    var Chile = {lat: -31.7613365, lng: -71.3187697};
+    
+    // The map, centered at World
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 1, center:new google.maps.LatLng(-9.9999999,69.9999999)});
+    // The marker, positioned on the places
+    var marker = new google.maps.Marker({position: South_Korea, map: map});
+    var marker = new google.maps.Marker({position: New_Zealand, map: map});
+    var marker = new google.maps.Marker({position: South_Africa, map: map});
+    var marker = new google.maps.Marker({position: Chile, map: map});
+  }
